2b03cbf1dbe27fc3d51210519e4f8f1d925d77ca
Yasmine <yasmine.khodja.96@gmail.com>
2019-04-17 11:19:28 +0200

    Tout

--- InterpolationPPV.cpp
+++ InterpolationPPV.cpp
@@ -13,7 +13,6 @@ using namespace std;
 
 int main(int argc, char **argv) {
     char cNomImgLue[250], cNomImgEcrite[250], nomMasque[250];
-    int iterations;
     if (argc != 4) {
         printf("Usage: ImageIn.pgm masque.pgm ImageOut.pgm\n");
         return 1;
@@ -22,39 +21,36 @@ int main(int argc, char **argv) {
     sscanf(argv[2], "%s", nomMasque);
     sscanf(argv[3], "%s", cNomImgEcrite);
     
-    ImageBase imageBase, masque;
-    ImageBase out(imageBase.getWidth(), imageBase.getHeight(), imageBase.getColor());
-    imageBase.load(cNomImgLue);
+    ImageBase masque;
     masque.load(nomMasque);
     
-    int cleanPixels[imageBase.getHeight() * imageBase.getWidth()];
-    int cpt = 0;
-    for (int i = 0; i < imageBase.getHeight(); ++i) {
-        for (int j = 0; j < imageBase.getWidth(); ++j) {
-            if (masque[i][j] == 0) {
-               cleanPixels[cpt] = imageBase[i][j];
-               cout << "cleanPixels[" << cpt << "] = " << cleanPixels[cpt] << endl;
-               cpt++;
-            }
-        }
-    }
-    
-    cout << "Taille de cleanPixels = " << cpt << endl;
+    ImageBase out(masque.getWidth(), masque.getHeight(), masque.getColor());
+    ImageBase imageBase;
+    imageBase.load(cNomImgLue);
     for (int i = 0; i < masque.getHeight(); ++i) {
         for (int j = 0; j < masque.getWidth(); ++j) {
             if (masque[i][j] == 255) {
-                int xk = (masque.getWidth() * i + j) / cpt, xk1 = (masque.getHeight() * i + j) / cpt + 1;
-                int x = (masque.getWidth() * i + j) / cpt;
-                if (x > (xk + xk1) / 2) {
-                    out[i][j] = static_cast<unsigned char>(cleanPixels[xk1 / cpt]);
-                } else {
-                    out[i][j] = static_cast<unsigned char>(cleanPixels[xk / cpt]);
+                double distMin = imageBase.getWidth();
+                int ppvI = i, ppvJ = j;
+                for (int k = 0; k < masque.getHeight(); ++k) {
+                    for (int l = 0; l < masque.getWidth(); ++l) {
+                        if (masque[k][l] == 0) {
+                            int x = abs(j - l);
+                            int y = abs(i - k);
+                            double dist = sqrt(pow(x, 2) + pow(y, 2));
+                            if (dist < distMin) {
+                                distMin = dist;
+                                ppvI = k;
+                                ppvJ = l;
+                            }
+                        }
+                    }
                 }
+                out[i][j] = imageBase[ppvI][ppvJ];
             } else {
                 out[i][j] = imageBase[i][j];
             }
         }
     }
-    
     out.save(cNomImgEcrite);
 }
